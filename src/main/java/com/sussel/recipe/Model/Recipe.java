33e4439f7b7133438902a65e89e019f1039f1ea7
Guilherme Sussel <guilherme.sussel@fast2mine.com>
2023-07-08 18:03:29 -0300

    fix id generation type in database

--- src/main/java/com/sussel/recipe/Model/Recipe.java
+++ src/main/java/com/sussel/recipe/Model/Recipe.java
@@ -2,13 +2,14 @@ package com.sussel.recipe.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Recipe {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
