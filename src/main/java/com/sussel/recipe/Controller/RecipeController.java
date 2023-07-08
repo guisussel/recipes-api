b348e7b8fddc38144c9ddd87b5b328794a3bf62e
Guilherme Sussel <guilherme.sussel@fast2mine.com>
2023-07-08 02:12:13 -0300

    code refactor

--- src/main/java/com/sussel/recipe/Controller/RecipeController.java
+++ src/main/java/com/sussel/recipe/Controller/RecipeController.java
@@ -2,26 +2,25 @@ package com.sussel.recipe.Controller;
 
 import com.sussel.recipe.DAO.RecipeRepository;
 import com.sussel.recipe.Model.Recipe;
+import com.sussel.recipe.Service.RecipeService;
 import com.sussel.recipe.View.RecipeView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/recipes")
 public class RecipeController {
 
-    private final RecipeRepository recipeRepository;
+    private final RecipeService recipeService;
     private final RecipeView recipeView;
 
     @Autowired
-    public RecipeController(RecipeRepository recipeRepository, RecipeView recipeView) {
-        this.recipeRepository = recipeRepository;
+    public RecipeController(RecipeService recipeService, RecipeView recipeView) {
+        this.recipeService = recipeService;
         this.recipeView = recipeView;
     }
 
@@ -32,14 +31,14 @@ public class RecipeController {
 
     @GetMapping
     public List<Recipe> getAllRecipes() {
-        return recipeRepository.findAll();
+        return recipeService.getAllRecipes();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Recipe> getRecipeById(@PathVariable Long id) {
-        Recipe receita = recipeRepository.findById(id).orElse(null);
-        if (receita != null) {
-            return new ResponseEntity<>(receita, HttpStatus.OK);
+        Recipe recipe = recipeService.getRecipeById(id);
+        if (recipe != null) {
+            return new ResponseEntity<>(recipe, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
@@ -47,9 +46,9 @@ public class RecipeController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Recipe> deleteRecipe(@PathVariable Long id) {
-        Recipe recipe = recipeRepository.findById(id).orElse(null);
+        Recipe recipe = recipeService.getRecipeById(id);
         if(recipe != null) {
-            recipeRepository.delete(recipe);
+            recipeService.deleteRecipe(recipe);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -58,7 +57,7 @@ public class RecipeController {
 
     @PostMapping
     public ResponseEntity<Recipe> saveRecipe(@RequestParam("name") String name) {
-        Recipe recipe = recipeRepository.save(new Recipe(name));
+        Recipe recipe = recipeService.saveRecipe(new Recipe(name));
         return new ResponseEntity<>(recipe, HttpStatus.CREATED);
     }
 
